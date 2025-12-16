7dd3156889c9785683658858b26db90c7d0c2680
hyunsik2000 <ryuhsik2000@naver.com>
2025-12-16 20:16:45 +0900

    네이버 로그인

--- login.js
+++ login.js
@@ -1,64 +1,62 @@
-const form = document.querySelector("form");
-const idInput = document.querySelector("#user_id");
-const passwordInput = document.querySelector("#user_pw");
+const kakaoLoginButton = document.querySelector("#kakao");
+const naverLoginButton = document.querySelector("#naver");
 
-const loginButton = document.querySelector("#login_btn");
+const userImage = document.querySelector("img");
+const userName = document.querySelector("#user_name");
 const logoutButton = document.querySelector("#logout_btn");
 
-const main = document.querySelector("main");
-const userName = document.querySelector("#user_name");
-const userDetail = document.querySelector("#user_info");
-
-axios.defaults.withCredentials = true;
-
-// form의 기본 submit 값 초기화
-form.addEventListener("submit", (e) => e.preventDefault());
-
-// 로그인 함수
-function login() {
-  const userId = idInput.value;
-  const userPassword = passwordInput.value;
-
-  return axios.post("http://localhost:3000", { userId, userPassword });
+function renderUserInfo(imgUrl, name) {
+  userImage.src = imgUrl;
+  userName.textContent = name;
 }
 
-// 로그아웃 함수
-function logout() {
-  return axios.delete("http://localhost:3000");
-}
+const naverClientId = "OR7fQEIiv0rcinYZk8jC";
+const naverClientSecret = "Aox_Lq6zxW";
+const naverSecret = "oz_coding";
+const redirectURI = "http://127.0.0.1:5500";
+let naverAccessToken = "";
 
-// 유저 정보를 받아오는 함수
-function getUserInfo() {
-  return axios.get("http://localhost:3000");
-}
-
-// 유저 정보를 렌더링 시키는 함수
-function renderUserInfo(userInfo) {
-  main.style.display = "block";
-  form.style.display = "none";
-  userName.textContent = userInfo.user_name;
-  userDetail.textContent = userInfo.user_info;
-}
-
-// 유저 정보를 숨기는 함수
-function renderLoginForm() {
-  main.style.display = "none";
-  form.style.display = "block";
-  userName.textContent = "";
-  userDetail.textContent = "";
-}
-
-// 로그인 버튼을 클릭하는 경우 post 요청 보낸 후 get 요청
-loginButton.onclick = () => {
-  login()
-    .then(() => getUserInfo())
-    .then((res) => renderUserInfo(res.data));
+kakaoLoginButton.onclick = () => {
+  location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${kakaoClientId}&redirect_uri=${redirectURI}&response_type=code`;
 };
 
-// 로그아웃 버튼을 클릭하는 경우
 logoutButton.onclick = () => {
-  logout().then((res) => {
-    console.log(res);
-    renderLoginForm();
-  });
+  axios
+    .delete("http://localhost:3000/naver/logout", {
+      data: { naverAccessToken },
+    })
+    .then((res) => {
+      renderUserInfo("", "");
+    });
+};
+
+window.onload = () => {
+  const url = new URL(location.href);
+  const urlParams = url.searchParams;
+  const authorizationCode = urlParams.get("code");
+  const naverState = urlParams.get("state");
+
+  if (authorizationCode) {
+    if (naverState) {
+      axios
+        .post("http://localhost:3000/naver/login", {
+          authorizationCode,
+        })
+        .then((res) => {
+          naverAccessToken = res.data;
+          return axios
+            .post("http://localhost:3000/naver/userInfo", {
+              naverAccessToken,
+            })
+            .then((res) => {
+              console.log(res.data);
+              renderUserInfo(res.data.profile_image, res.data.name);
+            });
+        });
+    }
+  }
+};
+
+naverLoginButton.onclick = () => {
+  location.href = `https://nid.naver.com/oauth2.0/authorize?client_id=${naverClientId}&response_type=code&redirect_uri=${redirectURI}&state=${naverSecret}`;
 };
--- server.js
+++ server.js
@@ -1,89 +1,50 @@
 const express = require("express");
 const cors = require("cors");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
+const axios = require("axios");
 
-// users의 정보를 확인하신 후에 로그인 요청을 진행해주세요.
-// server.js 파일 내에 총 5가지의 문제가 존재합니다.
-// 문제의 요구사항을 꼼꼼히 읽어보신 후에 과제를 진행해주세요.
-const users = [
-  {
-    user_id: "oz_user1",
-    user_password: "1234",
-    user_name: "김오즈",
-    user_info: "서울에 거주하는 김오즈입니다.",
-  },
-  {
-    user_id: "oz_user2",
-    user_password: "4567",
-    user_name: "박코딩",
-    user_info: "부산에 거주하는 박코딩입니다.",
-  },
-  {
-    user_id: "oz_user3",
-    user_password: "7890",
-    user_name: "이쿠키",
-    user_info: "경기에 거주하는 이쿠키입니다.",
-  },
-  {
-    user_id: "oz_user4",
-    user_password: "1357",
-    user_name: "최노드",
-    user_info: "제주에 거주하는 최노드입니다.",
-  },
-];
+const naverClientId = "OR7fQEIiv0rcinYZk8jC";
+const naverClientSecret = "Aox_Lq6zxW";
+const naverSecret = "oz_coding";
+const redirectURI = "http://127.0.0.1:5500";
 
 const app = express();
 
 app.use(
   cors({
-    // ⭐️ origin에 설정되어 있는 포트번호를 본인의 라이브서버 포트번호로 변경해주세요.
     origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
-    credentials: true,
   })
 );
 
-app.use(cookieParser());
 app.use(express.json());
 
-// 1️⃣. 요구사항에 맞도록 session 옵션을 설정해 주세요. (총 4가지)
-app.use(
-  session({
-    secret: "session secret", // 암호화, 열쇠 역할을 하는 문자열 설정
-    resave: false, // 요청이 들어왔을 때 변경되는 사항이 없는 경우 저장하지 않도록 설정
-    saveUninitialized: false, // 요청이 들어왔을 때 내용이 비어있는 경우 저장하지 않도록 설정
-    name: "session_id", // 쿠키 이름을 session_id로 변경
-  })
-);
-
-// POST 요청 (로그인 요청시 보내는 메소드)
-app.post("/", (req, res) => {
-  const { userId, userPassword } = req.body; // 2️⃣. 요청 바디에서 전달받은 값을 구조분해 할당을 사용하여 관리하세요.
-
-  const userInfo = users.find(
-    (el) => el.user_id === userId && el.user_password === userPassword
-  ); // 3️⃣. (find 메서드를 사용하여) users의 정보와 사용자가 입력한 정보를 비교하여 일치하는 회원이 존재하는지 확인하는 로직을 작성하세요.
-
-  if (!userInfo) {
-    res.status(401).send("로그인 실패");
-  } else {
-    req.session.userId = userInfo.user_id;
-    res.send("⭐️세션 생성 완료!");
-  }
+app.post("/naver/login", (req, res) => {
+  const authorizationCode = req.body.authorizationCode;
+  axios
+    .post(
+      `https://nid.naver.com/oauth2.0/token?client_id=${naverClientId}&client_secret=${naverClientSecret}&grant_type=authorization_code&state=${naverSecret}&code=${authorizationCode}`
+    )
+    .then((response) => res.send(response.data.access_token));
 });
 
-// GET 요청
-app.get("/", (req, res) => {
-  const userInfo = users.find((el) => el.user_id === req.session.userId); // json 형식으로 내보내기
-  return res.json(userInfo);
+app.post("/naver/userinfo", (req, res) => {
+  const { naverAccessToken } = req.body;
+  axios
+    .get("https://openapi.naver.com/v1/nid/me", {
+      headers: {
+        Authorization: `Bearer ${naverAccessToken}`,
+      },
+    })
+    .then((response) => res.json(response.data.response));
 });
 
-// DELETE 요청
-app.delete("/", (req, res) => {
-  req.session.destroy(); // 4️⃣. 세션 내 정보를 삭제하는 메소드를 작성하세요.
-  res.clearCookie("session_id"); // 5️⃣. 쿠키를 삭제하는 메소드를 작성하세요.
-  res.send("🧹세션 삭제 완료");
+app.delete("/naver/logout", (req, res) => {
+  const { naverAccessToken } = req.body;
+  axios
+    .post(
+      `https://nid.naver.com/oauth2.0/token?grant_type=delete&client_id=${naverClientId}&client_secret=${naverClientSecret}&access_token=${naverAccessToken}&service_provider=NAVER`
+    )
+    .then((response) => res.send("로그아웃 성공"));
 });
 
 app.listen(3000, () => console.log("서버 실행 ..."));
